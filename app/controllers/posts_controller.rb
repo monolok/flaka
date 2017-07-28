b784191ef4a2558e121295552adbf9376c80073a
monolok <antoinebertin@me.com>
2017-07-28 14:44:43 +0200

    basic

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,19 @@
+class PostsController < ApplicationController
+
+	def index
+		
+	end
+
+	def about
+		
+	end
+
+	def category
+		
+	end
+
+	def contact
+		
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "posts#index"
+  get "/flaka", to: "posts#about", as: "flaka"
+  get "/category", to: "posts#category", as: "category"
+  get "/contact", to: "posts#contact", as: "contact"
+
 end

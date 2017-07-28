47d74ad647a1079262b9ba0b98fd296cb4fd13fe
monolok <antoinebertin@me.com>
2017-07-28 19:32:10 +0200

    CRUD

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'bootstrap-sass'
 gem "font-awesome-rails"
+gem 'ckeditor', '~> 4.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "posts#index"
   get "/flaka", to: "posts#about", as: "flaka"
   get "/category", to: "posts#category", as: "category"
   get "/contact", to: "posts#contact", as: "contact"
   get "/video", to: "posts#video", as: "video"
+  resources :posts
 
 end
